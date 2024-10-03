fcb72b65e289df0dd7a46f85a4db17b487fb04ef
geo-ant <54497890+geo-ant@users.noreply.github.com>
2024-10-03 12:07:38 +0200

    add happy path tests for invariants

--- quick-builder-derive/tests/with_invariants.rs
+++ quick-builder-derive/tests/with_invariants.rs
@@ -14,22 +14,69 @@ struct WithOneGeneric<T: PartialEq>
 where
     T: std::fmt::Debug + Ord + Default,
 {
+    #[invariant(checks::check_foo)]
     foo: T,
     #[invariant(|me|me.as_ref().is_some_and(|v|*v>T::default()))]
     bar: Option<T>,
 }
 
-#[derive(PartialEq, Debug, QuickBuilder)]
-struct WithMultiGenericsAndLifetimes<'a, 'b: 'a, 'c, T, U: Clone>
+#[derive(Debug, PartialEq, QuickBuilder)]
+#[invariant(|this|this.width*this.height == this.data.len())]
+struct ImageMut<'a, T: Copy>
 where
-    T: Copy,
-    'c: 'b,
+    T: Default,
 {
-    first: &'a str,
-    second: &'b T,
-    third: &'c usize,
-    fourth: Option<(T, U)>,
+    width: usize,
+    #[invariant(|h|*h>0)]
+    height: usize,
+    data: &'a mut [T],
+}
+
+mod checks {
+    pub fn check_foo<T: Default + Ord>(t: &T) -> bool {
+        t < &Default::default()
+    }
 }
 
 #[test]
-fn happy_paths_for_builders_with_invariants() {}
+fn happy_paths_for_builders_with_invariants() {
+    let built = WithoutGenerics::builder()
+        .first(123)
+        .second(32.0)
+        .third("hello".into())
+        .build()
+        .expect("builder with valid invariants should not fail");
+    let expected = WithoutGenerics {
+        first: 123,
+        second: 32.0,
+        third: "hello".into(),
+    };
+    assert_eq!(built, expected);
+
+    let built = WithOneGeneric::builder()
+        .foo(-1)
+        .bar(Some(337))
+        .build()
+        .expect("builder with valid invariants should not fail");
+    let expected = WithOneGeneric {
+        foo: -1,
+        bar: Some(337),
+    };
+
+    assert_eq!(built, expected);
+
+    let mut data1 = [1, 2, 3, 4, 5, 6];
+    let mut data2 = [1, 2, 3, 4, 5, 6];
+    let built = ImageMut::builder()
+        .width(2)
+        .height(3)
+        .data(&mut data1)
+        .build()
+        .expect("builder with valid invariants should not fail");
+    let expected = ImageMut {
+        width: 2,
+        height: 3,
+        data: &mut data2,
+    };
+    assert_eq!(built, expected);
+}
